fe85b7b56fbf7bc0ced9525e4d4c172923fde6dd
Kaique Carvalho <102710926+Kai9ue@users.noreply.github.com>
2023-03-04 19:13:51 -0300

    nodemon, eslint & editorconfig

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  response.send("Hello")
+  response.send('Hello');
 });
 
-app.listen(3003, () => console.log('Its ok http://localhost:3003'))
+app.listen(3003, () => console.log('Its ok http://localhost:3003'));
