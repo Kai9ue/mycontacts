f7073098e53c454378d961ee448ee3dcc0e188db
Kaique Carvalho <102710926+Kai9ue@users.noreply.github.com>
2023-03-04 18:43:35 -0300

    criando rota com express

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+  response.send("Hello")
+});
+
+app.listen(3003, () => console.log('Its ok http://localhost:3003'))
